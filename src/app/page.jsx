1dd06cae1b5438558ea9d2815279f0d478ce70f5
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-10 14:58:26 +0530

    Add files via upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,1 +1,9 @@
-export default function Admin(){return(<div>Admin Panel</div>);}
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">PR</h1>
+      <h2 className="text-3xl mt-2">PRstudy</h2>
+      <p className="mt-3 text-orange-400">🇮🇳 INDIA YAAD RAKHE 🇮🇳</p>
+    </main>
+  );
+}

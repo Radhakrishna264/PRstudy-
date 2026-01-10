1654ff492032b197e7a24fbc2ffc50d50e72bc92
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-10 15:35:10 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">PR</h1>
-      <h2 className="text-3xl mt-2">PRstudy</h2>
-      <p className="mt-3 text-orange-400">🇮🇳 INDIA YAAD RAKHE 🇮🇳</p>
-    </main>
-  );
+  redirect("/login");
 }

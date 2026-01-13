96e67800bbabd196b0d83bc329a99fdb6171bcc5
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-13 12:52:21 +0530

    Update and rename page.jsx to index.js

---
+++ src/lib/permissions/index.js
@@ -0,0 +1,9 @@
+export const PERMISSIONS = {
+  VIEW_ADMIN: "view_admin",
+  MANAGE_USERS: "manage_users",
+  MANAGE_FILES: "manage_files",
+  MANAGE_TESTS: "manage_tests",
+  VIEW_PAYMENTS: "view_payments",
+  VIEW_ANALYTICS: "view_analytics",
+  SYSTEM_CONTROL: "system_control"
+};

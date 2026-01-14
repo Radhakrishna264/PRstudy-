67ddc88b9e9c64be131e0c8576b46b585af396e7
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-14 16:17:44 +0530

    Create index.js

---
+++ src/services/dm-core/index.js
@@ -0,0 +1,11 @@
+import { canStartDM } from "./consent";
+import { sendDM } from "./dmEngine";
+import { flagDM } from "./moderation";
+import { logDMAccess } from "./logs";
+
+export {
+  canStartDM,
+  sendDM,
+  flagDM,
+  logDMAccess,
+};

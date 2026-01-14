fd9a1ab6ebea80b28bbeeb2275cc02762576c15c
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-14 16:21:00 +0530

    Create index.js

---
+++ src/services/insights-engine/index.js
@@ -0,0 +1,9 @@
+import { aggregateMetrics } from "./aggregations/aggregate";
+import { createAlert } from "./alerts/alertEngine";
+import { exportReport } from "./exports/exportEngine";
+
+export {
+  aggregateMetrics,
+  createAlert,
+  exportReport,
+};

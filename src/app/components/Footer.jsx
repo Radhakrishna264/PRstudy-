5bf6a38f6785c5bf8f19b9d055730851f485861a
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-14 17:22:43 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,1 +1,21 @@
+import branding from "../../config/branding";
 
+export default function Footer() {
+  return (
+    <footer className="prstudy-footer">
+      <div className="footer-content">
+        <div className="footer-brand">
+          <strong>{branding.name}</strong>
+        </div>
+
+        <div className="footer-slogan">
+          {branding.slogan.text}
+        </div>
+
+        <div className="footer-copy">
+          © {new Date().getFullYear()} {branding.owner.name}
+        </div>
+      </div>
+    </footer>
+  );
+}

e180000fbc047e11fe7ff2b15c6c62483ebf0027
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-14 16:24:11 +0530

    Create index.js

---
+++ src/services/community-core/index.js
@@ -0,0 +1,11 @@
+import { createPost } from "./posts";
+import { reactToPost } from "./reactions";
+import { reportPost } from "./reports";
+import { moderatePost } from "./moderation";
+
+export {
+  createPost,
+  reactToPost,
+  reportPost,
+  moderatePost,
+};

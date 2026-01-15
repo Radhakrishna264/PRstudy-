030849ebf0c207cc3f699140717d5183719ed7fb
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-15 20:12:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
----
-
-# 3️⃣ `next.config.js`
-
-```js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
-
-  experimental: {
-    serverActions: true,
-  },
 };
 
 module.exports = nextConfig;
